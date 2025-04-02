4b0177de60e8a2fc4ad23619fcf56652b8ba7b4b
arise <ntbt662004@gmail.com>
2025-04-02 12:40:03 +0700

    note

--- backend/MyApi/obj/Debug/net9.0/MyApi.AssemblyInfo.cs
+++ backend/MyApi/obj/Debug/net9.0/MyApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a14bbddb2dd4bed44d6e9364dd4df3a810f85ae1")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
